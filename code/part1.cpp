1395d76736a3eeccfee4539397b693f405d2a502
Yukingce <yukingce@gmail.com>
2025-11-20 17:18:24 +0800

    add

---
+++ code/part1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//冒泡排序
+void main()
+{
+    int arr[9] = {4, 2, 8, 0, 5, 7, 1, 3, 9};
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8 - i; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+            }
+
+        }
+    }
+}
+
